c269c3d9475da51dc2f859a29f4dff31cb18699b
VoidStanza <hakshita2006@gmail.com>
2026-02-13 12:16:44 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const stories = [
+  { id: "dance", title: "Dance of the 2 Empires", available: true },
+  { id: "anamnesis", title: "Anamnesis", available: true },
+  { id: "empathodexterity", title: "Empathodexterity", available: true },
+  { id: "horizon", title: "Horizon", available: true },
+  { id: "untitled5", title: "Untitled V", available: false }
+];
